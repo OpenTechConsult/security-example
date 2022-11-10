8b23d75f01ecaad0c504c4118697500f3a61902e
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-10 12:31:02 +0000

    Installing Passport.js to handle OAuth authentication Keeping Client_ID and Client_Secret in a .env file Authenticating the app with Google and OAuth

--- server.js
+++ server.js
@@ -4,45 +4,79 @@ const https = require('https')
 
 const helmet = require('helmet')
 const express = require('express')
+const passport = require('passport')
+const { Strategy } = require('passport-google-oauth20')
 
+require('dotenv').config()
 
 const PORT = 3000
+
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+}
+
+const AUTH_OPTIONS = {
+    callbackURL: '/auth/google/callback',
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,
+}
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+    console.log('Google profile ', profile)
+    done(null, profile)
+    // TODO: perfect place to make any comparisons or save the user profile in a database
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
 
 const app = express()
 
 app.use(helmet())
+app.use(passport.initialize())
 
 function checkLoggedIn(req, res, next) {
     // TODO:
     const isLoggedIn = true
 
     if (!isLoggedIn) {
-        return res.status(401).json({ 
+        return res.status(401).json({
             error: 'You must log in!'
         })
     }
     next()
 }
 
-app.get('/auth/google', (req, res) => {
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['email']
+}))
 
-})
-
-app.get('/auth/google/callback', (req, res) => {
-    
-})
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/failure',
+        successRedirect: '/',
+        session: false,
+    }),
+    (req, res) => {
+        console.log('Google called us back!')
+    })
 
 app.get('/auth/logout', (req, res) => {
-    
+
 })
 
 app.get('/secret', checkLoggedIn, (req, res) => {
     return res.send('Your personal secret value is 42')
 })
 
+app.get('/failure', (req, res) => {
+    return res.send('Failed to log in')
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
 
 https
     .createServer({
